8cf2bd9068bad8390369efe67ec640aed23bc608
liuqing <1229674380@qq.com>
2021-01-18 11:38:09 +0800

    管理端运行实验弹出框添加

--- src/views/admin/experimentManage.vue
+++ src/views/admin/experimentManage.vue
@@ -15,7 +15,7 @@
           </div>
         </div>
         <div class="fr">
-          <a class="btnDefault pointer abtn" >一键释放</a>
+          <a class="btnDefault pointer abtn" @click="click_Release(1,'')">一键释放</a>
           
           
           <div class="d-serach"> 
@@ -35,7 +35,7 @@
                       <p class="p_id textline1">ID：{{item.id}}</p>
                       <p class="p_id textline1">用户：{{item.username}}</p>
                      <div class="btnbox">
-                         <a class="btnDefault machbtn pointer"><span>释放内存</span></a>
+                         <a class="btnDefault machbtn pointer" @click="click_Release(2,item.id)"><span>释放内存</span></a>
                      </div>
                  </div>
              </li>
@@ -54,6 +54,29 @@
         </div>
       </div>
     </div>
+   
+    <!--虚拟机内存释放弹出框-->
+    <el-dialog
+      
+      :visible.sync="show_Release"
+      width="600">
+      <div slot="title" class="dialog_header">
+          请注意!
+      </div>
+      <div class="confirm_dialog_body">
+          <p class="dialog_mess" v-if="!release_success"><span class="span_icon icon_waring">{{dialog_machine}}</span></p>
+          <p class="dialog_mess" v-if="release_success"><span class="span_icon icon_success">虚拟机内存已释放！</span></p>
+      </div>
+      <div slot="footer" class="dialog-footer " v-if="!release_success">
+          <a class="btnDefault" @click="confiremRelease">确 认</a>
+          <a class="btnDefault"   @click="release_success = false;show_Release=false">取 消</a>
+      </div>
+        <div slot="footer" class="dialog-footer " v-if="release_success">
+          <a class="btnDefault"  @click="successConfirm">确 认</a>
+    
+      </div>
+      </el-dialog>
+
   </div>
 </template>
 <script>
@@ -83,7 +106,10 @@ export default {
       ],//设备列表
       showDel: false, //删除多选是否显示
       perPage:24, //虚拟机每页
-      curPage:1
+      curPage:1,
+      show_Release:false,//虚拟机释放弹出框显示
+      release_success:false,//是否是否成功
+      dialog_machine:'',
     };
   },
 
@@ -127,6 +153,29 @@ export default {
     },
     upLoad() {
       this.$refs.upload.active = true;
+    },
+    //虚拟机释放点击
+    click_Release(num,id){
+      let that = this;
+      that.show_Release = true
+      that.release_success = false
+      if(num==1){
+        that.dialog_machine ='确定要一键释放所有内存吗？'
+      }else{
+        that.dialog_machine ='确定要释放ID：'+id+'的虚拟机内存吗？'
+      }
+
+    },
+    //虚拟机释放确认
+    confiremRelease(){
+      let that = this;
+      that.release_success = true
+    },
+    //释放成功确认
+    successConfirm(){
+      let that = this
+       that.show_Release = false
+      that.release_success = false
     }
    
   },
