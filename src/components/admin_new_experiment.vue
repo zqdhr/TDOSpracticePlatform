cb45cd801bbaedc1f6f6ab07e729cafa6b32b961
liuqing <1229674380@qq.com>
2021-02-04 09:55:13 +0800

    新增样式调整

--- src/components/admin_new_experiment.vue
+++ src/components/admin_new_experiment.vue
@@ -280,9 +280,7 @@
 <script>
 import FileUpload from "vue-upload-component";
 import { quillEditor } from "vue-quill-editor"; //调用编辑器
-import 'quill/dist/quill.core.css';
-import 'quill/dist/quill.snow.css';
-import 'quill/dist/quill.bubble.css';
+
 import {findParentCategory,findChildCategory,insertExperiment,getImagequoteList,upload} from '@/API/api';
 export default {
   data() {
--- src/views/experiment.vue
+++ src/views/experiment.vue
@@ -107,9 +107,7 @@ import RFB from '@novnc/novnc/core/rfb';
 
 
 import { quillEditor } from "vue-quill-editor"; //调用编辑器
-import 'quill/dist/quill.core.css';
-import 'quill/dist/quill.snow.css';
-import 'quill/dist/quill.bubble.css';
+
 
  import html2canvas from 'html2canvas';
 
