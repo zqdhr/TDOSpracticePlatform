62c313f93d5c015155a43aadfdb35c60d52ffa68
liuqing <1229674380@qq.com>
2021-02-20 10:22:57 +0800

    1

--- src/views/experiment1.vue
+++ src/views/experiment1.vue
@@ -223,28 +223,27 @@ export default {
         }  
     },
     mounted() {
-         window.addEventListener('load', this.handleMessage) // 监听iframe的事件
-         this.iframeWin = this.$refs.frameWrapper
-         //console.log(this.iframeWin)
-         console.log('123'+this.$refs.frameWrapper.contentWindow)
+     
     },
     methods:{
-         handleMessage(event) {
+      handleMessage(event) {
         let data=event.data
         console.log('123')
       },
         // vnc连接断开的回调函数
         disconnectedFromServer (msg) {
           console.log(msg)
+          console.log('连接断开的原因')
             if(msg.detail.clean){
                 // 根据 断开信息的msg.detail.clean 来判断是否可以重新连接
-                this.contentVnc()
+                this.connectVnc()
             } else {
                 //这里做不可重新连接的一些操作
             }
         },
         // 连接成功的回调函数
         connectedToServer(msg) {
+            console.log('成功')
             console.log('success',msg)
         },
            //创建实验
@@ -410,11 +409,11 @@ export default {
             
             let rfb = new RFB(document.getElementById('screen'), url, { 
             // 向vnc 传递的一些参数，比如说虚拟机的开机密码等,wsProtocols:'binary'
-                credentials: {password: '123456' }
+                credentials: {password: '123456'}
             });
             rfb.addEventListener('connect', this.connectedToServer);
             rfb.addEventListener('disconnect', this.disconnectedFromServer);
-            rfb.scaleViewport = true;  //scaleViewport指示是否应在本地扩展远程会话以使其适                    合其容器。禁用时，如果远程会话小于其容器，则它将居中，或者根据clipViewport它是否更大来处理。默认情况下禁用。
+            rfb.scaleViewport = true;  //scaleViewport指示是否应在本地扩展远程会话以使其适 合其容器。禁用时，如果远程会话小于其容器，则它将居中，或者根据clipViewport它是否更大来处理。默认情况下禁用。
             rfb.resizeSession = true; //是一个boolean指示是否每当容器改变尺寸应被发送到调整远程会话的请求。默认情况下禁用
             console.log(rfb)
             this.rfb = rfb;
